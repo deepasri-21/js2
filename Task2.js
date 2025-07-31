3d314b5f326887650ba4b943962f3631e1996f00
deepasri-21 <144312010+deepasri-21@users.noreply.github.com>
2025-07-31 23:09:19 +0530

    Update Task2.js

--- Task2.js
+++ Task2.js
@@ -19,14 +19,14 @@ let x = 5;
 x += 2;   // x = x + 2 → x = 7
 x *= 3;   // x = x * 3 → x = 21
 
-console.log("Value of x after += and *= : " + x);
+console.log("Value of x : " + x);
 
 // Comparison Operators (==, ===, !=, !==, >, <, >=, <=)
 let y = 10;
 let z = '10';
 
-console.log("y == z: " + (y == z));   // true (only value compared)
-console.log("y === z: " + (y === z)); // false (value & type compared)
+console.log("y == z: " + (y == z));   // true 
+console.log("y === z: " + (y === z)); // false 
 console.log("y != z: " + (y != z));   // false
 console.log("y !== z: " + (y !== z)); // true
 console.log("y > 5: " + (y > 5));     // true
