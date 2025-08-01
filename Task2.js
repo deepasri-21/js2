2f6bafe8089f5981e45b6ff4db69b0abdb597d3b
deepasri-21 <144312010+deepasri-21@users.noreply.github.com>
2025-08-01 20:13:41 +0530

    Update Task2.js

--- Task2.js
+++ Task2.js
@@ -1,31 +1,31 @@
-    let a = 10;
-    let b = 3;
+let a = 10;
+let b = 3;
 
-    let sum = a + b;         
-    let difference = a - b;  
-    let product = a * b;     
-    let quotient = a / b;    
-    let remainder = a % b;   
+let sum = a + b;         
+let difference = a - b;  
+let product = a * b;     
+let quotient = a / b;    
+let remainder = a % b;   
 
-    document.write("Sum: " + sum + "<br>");
-    document.write("Difference: " + difference + "<br>");
-    document.write("Product: " + product + "<br>");
-    document.write("Quotient: " + quotient + "<br>");
-    document.write("Remainder: " + remainder + "<br>");
+document.write("Sum: " + sum + "<br>");
+document.write("Difference: " + difference + "<br>");
+document.write("Product: " + product + "<br>");
+document.write("Quotient: " + quotient + "<br>");
+document.write("Remainder: " + remainder + "<br>");
 
-    let x = 5;
-    x += 2;   
-    x *= 3;   
+let x = 5;
+x += 2;   
+x *= 3;   
 
-    document.write("Value of x: " + x + "<br>");
+document.write("Value of x: " + x + "<br>");
 
-    let y = 10;
-    let z = '10';
+let y = 10;
+let z = '10';
 
-    document.write("y == z: " + (y == z) + "<br>");
-    document.write("y === z: " + (y === z) + "<br>");
-    document.write("y != z: " + (y != z) + "<br>");
-    document.write("y !== z: " + (y !== z) + "<br>");
-    document.write("y > 5: " + (y > 5) + "<br>");
-    document.write("y <= 10: " + (y <= 10) + "<br>");
+document.write("y == z: " + (y == z) + "<br>");
+document.write("y === z: " + (y === z) + "<br>");
+document.write("y != z: " + (y != z) + "<br>");
+document.write("y !== z: " + (y !== z) + "<br>");
+document.write("y > 5: " + (y > 5) + "<br>");
+document.write("y <= 10: " + (y <= 10) + "<br>");
   
